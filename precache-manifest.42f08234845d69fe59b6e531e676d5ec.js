d524bd5b2d7ac817b76d3b108122444383f4a509
Arthur Atul <arthur.atul@ithnads.biz>
2020-01-29 16:11:14 +0530

    Updates

---
+++ precache-manifest.42f08234845d69fe59b6e531e676d5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eb56339863da2390a5d8586cdeb490",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cd57a4ef214889da261f",
+    "url": "/monster-rolodex/static/css/main.16632c12.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd57a4ef214889da261f",
+    "url": "/monster-rolodex/static/js/main.f29326a6.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
